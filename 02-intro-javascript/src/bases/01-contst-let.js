48ceb2c34bc1b4b62b1a11e6d0dee60b634baafa
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-11-29 00:53:19 -0600

    let y const

---
+++ 02-intro-javascript/src/bases/01-contst-let.js
@@ -0,0 +1,16 @@
+//variables y constantes
+
+const nombre = 'Jonathan';
+const apellido = 'Ruiz';
+
+let valorDado = 5;
+valorDado = 4;
+
+
+console.log(nombre, apellido, valorDado);
+
+if(true) {
+    const nombre = 'Ramiro'
+    console.log(nombre);
+}
+console.log(valorDado);
