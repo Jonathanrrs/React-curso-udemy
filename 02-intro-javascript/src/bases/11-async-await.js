8ae72b76a9f1cf9f69b143950a7959683201471c
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-05 00:29:04 -0600

    async await

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,34 @@
+// const getImagenPromesa = () => new Promise((resolve) => resolve('https://fgrfgfgf.com'));
+// getImagenPromesa().then(console.log);
+
+const getImagen = async() => {
+
+    try {
+        const apiKey = '';
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const { data }= await resp.json();
+        const {url} = data.images.original;
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    } catch (error) {
+        /* manejo del error */
+        console.error(error)
+    }
+    
+
+}
+
+getImagen();
+
+
+
+
+
+
+    
+    
+
+
+
+    
