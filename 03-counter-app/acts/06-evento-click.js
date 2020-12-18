d9f6c7d65f3d068c64a7302b824036eef1e64824
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-17 18:25:29 -0600

    onclick

---
+++ 03-counter-app/acts/06-evento-click.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({value}) => {
+
+    /* handleAdd */
+    const handleAdd = (e) => {
+        console.log(e);
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{value}</h2>
+        
+            {/* <button onClick={(e) => {handleAdd(e)} }>+1</button> */}
+            <button onClick={handleAdd}>+1</button> {/* es lo mismo */}
+        </>
+    )
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
+
+export default CounterApp;
