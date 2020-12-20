4e0d530c44f71b04ae6aee2072bb889a8a1a1ad8
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-20 14:50:14 -0600

    test con jest

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,20 +1,25 @@
-// import React from 'react';
-// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-/* Funcional components */
 
-const PrimeraApp = (props) => {
-    console.log(props);
-    const saludo = 'Hola mundo';
-
+const PrimeraApp = ({saludo, subtitulo}) => {
+  
     return (
-        <> {/* fragment */}
+        <>
             <h1>{saludo}</h1>
-            {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
-            <p>Mi primera aplicación</p>
+            <p>{subtitulo}</p>
         </>
-    );
-    
+    ); 
+}
+
+PrimeraApp.propTypes = {
+    /* puede ser lo que se necesita */
+    saludo: PropTypes.string.isRequired
+}
+
+/* valores por defecto para las propiedades */
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
 }
 
 export default PrimeraApp;
