4e34fcdd2ed2b769e6670479cbd54471da00f754
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-21 23:18:01 -0600

    useEffect

--- 04-gif-expert-app/src/componentes/GifGrid.js
+++ 04-gif-expert-app/src/componentes/GifGrid.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        getGifs();
+    }, [])
 
     const getGifs = async() => {
         const url = `https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=vD2873pCOTBaVMmRB83BYa1KkLi62PhF`
@@ -16,11 +22,11 @@ export const GifGrid = ({category}) => {
         console.log(gifs);
     }
 
-    getGifs();
-
     return (
         <div>
             <h3>{category}</h3>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count+1)}></button>
         </div>
     )
 }
