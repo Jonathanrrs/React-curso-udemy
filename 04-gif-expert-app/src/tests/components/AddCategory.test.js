7a626d02a250ad2275e8cfa0cf59e2f153623c7d
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-29 18:02:46 -0600

    simular cambios en imput

--- 04-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 04-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -5,10 +5,24 @@ import { AddCategory } from '../../componentes/AddCategory';
 describe('Pruebas en AddCategory', () => {
     
     const setCategories = () => {};
+    const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    
     test('Debe mostrarse correctamente ', () => {
         
-        const wrapper = shallow(<AddCategory setCategories={setCategories}/>);
         expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe cambiar la caja de texto', () => {
+        
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+        input.simulate('change', {target: {value}});  /* simular evento del input */
+
+        expect(wrapper.find('p').text().trim()).toBe(value);
+
+
     })
+    
+
     
 })
