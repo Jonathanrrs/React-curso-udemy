a355c8d86815591e84a7b240ead8709a9eb28558
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2020-12-31 18:54:57 -0600

    useFetch + useCounter

--- 05-hook-app/src/hooks/useFetch.js
+++ 05-hook-app/src/hooks/useFetch.js
@@ -4,6 +4,7 @@ export const useFetch = (url) => {
    
     const [state, setstate] = useState({data: null, loading: true, error: null});
     useEffect(() => {
+        setstate({data:null, loading: true, error: null});
         fetch(url)
             .then(res => res.json())
             .then(data => {
