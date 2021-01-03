5164d345257678675adeb77318ca8bb46a53d985
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-02 23:07:29 -0600

    Marcar como completado o pendiente un TODO

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -27,6 +27,12 @@ export const TodoApp = () => {
         dispatch(action);
     }
 
+    const handleToggle = (todoId) => {
+        dispatch({
+            type: 'toggle',
+            payload: todoId
+        })
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -62,7 +68,9 @@ export const TodoApp = () => {
                                     key={todo.id}
                                     className="list-group-item"
                                 >
-                                    <p className="text-center complete">{i + 1}. {todo.desc}</p>
+                                    <p className={`${todo.done && 'complete'}`}     
+                                        onClick={() => handleToggle(todo.id)}
+                                    >{i + 1}. {todo.desc}</p>
                                     <button
                                         className="btn btn-danger"
                                         onClick={() => handleDelete(todo.id)}
