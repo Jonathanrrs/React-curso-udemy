58a9d698913feb40a4f6848be056dc01eae64fb5
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-03 17:32:44 -0600

    Optimización #1

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react';
 import { todoReducer } from './todoReducer';
 import './styles.css';
 import { UseForm } from '../../hooks/UseForm';
+import { TodoList } from './TodoList';
 
 const init = () => {
     return JSON.parse(localStorage.getItem('todos')) || [];
@@ -11,7 +12,7 @@ export const TodoApp = () => {
 
     const [todos, dispatch] = useReducer(todoReducer, [], init); /* por defecto es un arreglo vacio */
 
-    const [{description}, handleInputChange, reset] = UseForm({
+    const [{ description }, handleInputChange, reset] = UseForm({
         description: '', /* este viene del formulario de abajo */
     });
 
@@ -37,7 +38,7 @@ export const TodoApp = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(description.trim().length <= 1) {
+        if (description.trim().length <= 1) {
             return;
         }
         const newTodo = {
@@ -61,47 +62,33 @@ export const TodoApp = () => {
             <hr />
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map((todo, i) => (
-                                <li
-                                    key={todo.id}
-                                    className="list-group-item"
-                                >
-                                    <p className={`${todo.done && 'complete'}`}     
-                                        onClick={() => handleToggle(todo.id)}
-                                    >{i + 1}. {todo.desc}</p>
-                                    <button
-                                        className="btn btn-danger"
-                                        onClick={() => handleDelete(todo.id)}
-                                    >
-                                        Borrar
-                        </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                    <TodoList 
+                        todos={todos} 
+                        handleDelete={handleDelete}
+                        handleToggle={handleToggle}
+                    
+                    />
                 </div>
                 <div className="col-5">
-                        <h4>Agregar TODO</h4>
-                        <hr />
-                        <form onSubmit={handleSubmit}>
-                            <input 
-                                type="text"
-                                name="description"
-                                className="form-control"
-                                placeholder="Aprender..."
-                                autoComplete="off"
-                                onChange={handleInputChange}
-                                value={description}
-                            />
-                            <button
-                                type="submit"
-                                className="btn btn-outline-primary mt-1 btn-block"
-                            >
-                                Agregar
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="text"
+                            name="description"
+                            className="form-control"
+                            placeholder="Aprender..."
+                            autoComplete="off"
+                            onChange={handleInputChange}
+                            value={description}
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-1 btn-block"
+                        >
+                            Agregar
                             </button>
-                        </form>
+                    </form>
                 </div>
             </div>
         </div>
--- 05-hook-app/src/components/08-useReducer/todoReducer.js
+++ 05-hook-app/src/components/08-useReducer/todoReducer.js
@@ -5,17 +5,6 @@ export const todoReducer = (state = [], action) => {
             return [...state, action.payload];
         case 'delete':
             return state.filter(todo => todo.id !== action.payload);
-        // case 'toggle':
-        //     return state.map((todo) => {
-        //         if(todo.id === action.payload) {
-        //             return {
-        //                 ...todo,
-        //                 done: !todo.done
-        //             }
-        //         } else {
-        //             return todo;
-        //         }
-        //     })
         case 'toggle':
                 return state.map(todo => 
                         (todo.id === action.payload)
