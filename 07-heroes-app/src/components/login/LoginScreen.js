e6f0dc5f71f752f4736c58883a289a595ad9d781
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-29 16:34:34 -0600

    History push / replace

--- 07-heroes-app/src/components/login/LoginScreen.js
+++ 07-heroes-app/src/components/login/LoginScreen.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-export const LoginScreen = () => {
+export const LoginScreen = ({history}) => {
+    
+    const handleLogin = () => {
+        /* history lo obtenemos de las props que proporciona react router, con el cual con el push podemos navegar a una ruta especifica */
+        // history.push('/');
+        /* utilizamos replace para que el back del navegador ya no vuelva al login porque ya lo hicimos, esa es la diferencia con push */
+        history.replace('/');
+    }
+    
     return (
-        <div>
-            <h1>Login Screen</h1>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+            <button
+                className="btn btn-primary"
+                onClick={handleLogin}
+            >
+                Login
+            </button>
         </div>
     )
 }
