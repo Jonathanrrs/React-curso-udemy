298a06f2affd44138958c1e226ec5f7f1fad789e
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-29 20:05:47 -0600

    Animaciones en nuestro componente

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -45,7 +45,7 @@ export const HeroScreen = ({history}) => {
                 <img 
                     src={`..//assets/heroes/${heroeId}.jpg`}
                     alt={superhero}
-                    className="img-thumbnail"
+                    className="img-thumbnail animate__animated animate__fadeInLeft"
                 >
                 </img>
                 
