418dd14f1d4ab892773adffe4381b8cafdfc869c
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-29 18:11:05 -0600

    Tarjetas con la información del Héroe

--- 07-heroes-app/src/components/heroes/HeroList.js
+++ 07-heroes-app/src/components/heroes/HeroList.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({publisher}) => {
     /* publisher viene de los selectos donde se manejó la data ficticia */
     const heroes = getHeroesByPublisher(publisher);
    
     return (
-        <ul>
+        <div className="card-columns">
             {
                 heroes.map(hero => (
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard 
+                        key={hero.id}
+                        /* usamos el operador spread para extraer las propiedades del heroe y esas van a hacer las nuevas propiedades que pasamos a herocard */
+                        {...hero}
+                    >
+                    </HeroCard>
                 ))
             }
-        </ul>
+        </div>
     )
 }
