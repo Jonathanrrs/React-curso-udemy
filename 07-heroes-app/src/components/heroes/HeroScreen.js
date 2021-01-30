a143cd7908ee6f540aceae47f8eac423ba6d1c04
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-29 19:49:48 -0600

    Estilo del componente HeroScreen

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroById } from '../../selectors/getHeroById';
 
-export const HeroScreen = () => {
+export const HeroScreen = ({history}) => {
     
     /* hook de react router, este extrae los parametros que vayan por el url */
     const {heroeId} = useParams();
@@ -12,6 +12,22 @@ export const HeroScreen = () => {
     if(!hero) {
         return <Redirect to="/" />
     }
+
+    const handleReturn = () => {
+        /* esta validación es para que con el goback() nos regresa en caso de un ruta especifica nos saca de la app, pero con la validacion 
+           utilizamos el push para prevenir esto
+        */
+        // if(history.length <=2) {
+        //     history.push('/');
+        // } else{
+        //     /* para regresar a una página */
+        //     history.goBack();
+        // }
+
+        /* es lo mismo pero más cool */
+        history.length <= 2 ? history.push('/') : history.goBack();
+    }
+
     const {
         superhero,
         publisher,
@@ -20,8 +36,31 @@ export const HeroScreen = () => {
         characters,
     } = hero;
     return (
-        <div>
-            <h1>Hero Screen</h1>
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    src={`..//assets/heroes/${heroeId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail"
+                >
+                </img>
+                
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego: {alter_ego}</b></li>
+                    <li className="list-group-item"><b>Publisher: {publisher}</b></li>
+                    <li className="list-group-item"><b>First appearance: {first_appearance}</b></li>
+                </ul>
+                <h5>characters</h5>
+                <p>{characters}</p>
+                <button className="btn btn-outline-info"
+                    onClick={handleReturn}
+                >
+                    Return
+                </button>
+            </div>
         </div>
     )
 }
--- 07-heroes-app/src/selectors/getHeroById.js
+++ 07-heroes-app/src/selectors/getHeroById.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import { heroes } from '../data/heroes'
 export const getHeroById = (id) => {
     
