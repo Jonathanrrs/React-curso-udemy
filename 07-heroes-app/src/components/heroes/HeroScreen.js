d5df797fc5961f65afe39bae3194e255827fd301
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-29 18:32:42 -0600

    Leer argumentos por URL

--- 07-heroes-app/src/components/heroes/HeroScreen.js
+++ 07-heroes-app/src/components/heroes/HeroScreen.js
@@ -1,6 +1,24 @@
 import React from 'react'
+import { Redirect, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+    
+    /* hook de react router, este extrae los parametros que vayan por el url */
+    const {heroeId} = useParams();
+    const hero = getHeroById(heroeId);
+
+    /* si el hereo no existe se redireccionará al home(marvel), no puede ser return porque un componente debe regresar algo, en ese caso un componente de react router  */
+    if(!hero) {
+        return <Redirect to="/" />
+    }
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
     return (
         <div>
             <h1>Hero Screen</h1>
