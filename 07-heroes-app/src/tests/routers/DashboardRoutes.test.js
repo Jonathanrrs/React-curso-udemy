f3ff1c1a4ed1c01682a08b83629e48bc0bc92cb8
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-02-11 17:06:11 -0600

    Pruebas en el componente DashboardRoutes

---
+++ 07-heroes-app/src/tests/routers/DashboardRoutes.test.js
@@ -0,0 +1,28 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
+import { DashboardRoutes } from '../../routers/DashboardRoutes';
+
+describe('Pruebas en DashboardRoutes', () => {
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged: true,
+            name: 'Ramiro'
+        }
+    }
+    test('Debe mostrarse correctamente ', () => {
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter>
+                <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.text-info').text().trim()).toBe('Ramiro');
+    });
+    
+})
