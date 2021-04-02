e111a7cb2a68aaeb5d045141768b642243e0ef5a
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-02 16:24:27 -0600

    NoteAppBar

---
+++ journal-app/src/components/notes/NotesAppBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+    return (
+        <div className="notes__appbar">
+            <span>2 de abril de 2020</span>
+            <div>
+                <button className="btn">
+                    Picture
+                </button>
+                <button className="btn">
+                    Save
+                </button>
+            </div>
+        </div>
+    )
+}
