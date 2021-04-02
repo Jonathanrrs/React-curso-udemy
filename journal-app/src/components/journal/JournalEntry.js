2f7a88fbf8e6eb7b9a0bc5c7603393bf0078eb9a
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-02 16:00:15 -0600

    Sidebar - JournalEntries

---
+++ journal-app/src/components/journal/JournalEntry.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className="journal__entry pointer">
+            <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://www.todofondos.net/wp-content/uploads/todofondos-spiderman2.jpg)'
+                }}
+            >
+            </div>
+            <div className="journal__entry-body">
+                <p className="journal__entry-title">
+                    Un nuevo día
+                </p>
+                <p className="journal__entry-content">
+                    Hola a todos desde React con SASS
+                </p>
+            </div>
+            <div className="journal__entry-date-box">
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+        </div>
+    )
+}
