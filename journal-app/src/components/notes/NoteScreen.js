f0ee46465d9edf28e38cf32146f81cc68dbdee3a
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-02 16:43:55 -0600

    Estructura y diseño del HomeScreen

--- journal-app/src/components/notes/NoteScreen.js
+++ journal-app/src/components/notes/NoteScreen.js
@@ -5,6 +5,26 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                >
+                </textarea>
+                <div>
+                    <img
+                        src="https://sm.ign.com/ign_es/screenshot/default/miles-morales-in-spider-man-into-the-spider-verse-2560x1440_qwyz.jpg"
+                        alt="img"
+                        className="notes__image"
+                    />
+                </div>
+            </div>
         </div>
     )
 }
