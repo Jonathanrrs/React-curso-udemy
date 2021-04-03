2cbc41411e531869cbf05e6debd995304cf84b4b
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-02 19:15:39 -0600

    Thunk Middleware - Acciones asíncronas

--- journal-app/src/store/store.js
+++ journal-app/src/store/store.js
@@ -1,5 +1,11 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import { authReducer } from '../reducers/authReducer';
+
+import thunk from 'redux-thunk';
+/* esto es para habilitar las extensiones de devtools y aplicar middleware */
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+
+
 
 /* createStore es para crear nuestro store */
 /* combinereducers es para poner los reducers que utilzaremos porque por defecto solo recibe uno createstore */
@@ -11,5 +17,7 @@ const reducers = combineReducers({
 /* necesitamos importar el store en el punto más alto de nuestra app, en journalapp */
 export const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk) /* trabajar acciones asincronas */
+    )
 );
