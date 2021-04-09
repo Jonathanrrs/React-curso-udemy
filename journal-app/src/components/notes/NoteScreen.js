e1508550b65c86f69aa40ba73d6a653c196249cc
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-08 20:56:31 -0500

    Activar una nota para su edición

--- journal-app/src/components/notes/NoteScreen.js
+++ journal-app/src/components/notes/NoteScreen.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { UseForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
+    /* renombrar */
+    const { active: note } = useSelector(store => store.notes)
+
+    const [values, handleInputChange] = UseForm(note);
+
+    const { body, title } = values;
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
@@ -12,19 +21,27 @@ export const NoteScreen = () => {
                     className="notes__title-input"
                     autoComplete="off"
                     autoFocus
+                    value={title}
+                    onChange={handleInputChange}
+
                 />
                 <textarea
                     placeholder="What happened today"
                     className="notes__textarea"
+                    value={body}
+                    onChange={handleInputChange}
                 >
                 </textarea>
-                <div>
-                    <img
-                        src="https://sm.ign.com/ign_es/screenshot/default/miles-morales-in-spider-man-into-the-spider-verse-2560x1440_qwyz.jpg"
-                        alt="img"
-                        className="notes__image"
-                    />
-                </div>
+                {
+                    (note.url) &&
+                    (<div>
+                        <img
+                            src="https://sm.ign.com/ign_es/screenshot/default/miles-morales-in-spider-man-into-the-spider-verse-2560x1440_qwyz.jpg"
+                            alt="img"
+                            className="notes__image"
+                        />
+                    </div>)
+                }
             </div>
         </div>
     )
