2b6f6f140580a365f9d0cc87490a219338ef7725
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-11 19:11:39 -0500

    Crear base de datos de desarrollo

--- journal-app/src/firebase/firebase-config.js
+++ journal-app/src/firebase/firebase-config.js
@@ -11,7 +11,24 @@ const firebaseConfig = {
     appId: "1:810608721114:web:e5aafacac23c4917a3b9e3"
 };
 
-firebase.initializeApp(firebaseConfig);
+const firebaseConfigTesting = {
+    apiKey: "AIzaSyA5bBx4AirIyDeXjSvblm9FLJooSzggQUs",
+    authDomain: "react-test-app-curso.firebaseapp.com",
+    projectId: "react-test-app-curso",
+    storageBucket: "react-test-app-curso.appspot.com",
+    messagingSenderId: "805099183686",
+    appId: "1:805099183686:web:6c224813eb7893721db447"
+  };
+
+if(process.env.NODE_ENV === 'test') {
+    // testing
+    firebase.initializeApp(firebaseConfigTesting);
+
+} else{
+    // dev/prod
+    firebase.initializeApp(firebaseConfig);
+}
+
 
 /* es la base de datos */
 const db = firebase.firestore();
