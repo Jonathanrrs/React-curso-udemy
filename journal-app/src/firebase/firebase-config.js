b7b1c8d4c652332b0f00b8028dd3a53823affb37
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-11 19:46:33 -0500

    Variables de entorno

--- journal-app/src/firebase/firebase-config.js
+++ journal-app/src/firebase/firebase-config.js
@@ -3,31 +3,15 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA22DB4HWV5Z3bZxdGN5_DiPrDnYtG9Acs",
-    authDomain: "react-app-cursos-72468.firebaseapp.com",
-    projectId: "react-app-cursos-72468",
-    storageBucket: "react-app-cursos-72468.appspot.com",
-    messagingSenderId: "810608721114",
-    appId: "1:810608721114:web:e5aafacac23c4917a3b9e3"
+    apiKey: process.env.REACT_APP_APIKEY, 
+    authDomain: process.env.REACT_APP_AUTHDOMAIN, 
+    projectId: process.env.REACT_APP_PROJECTID, 
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET, 
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID, 
+    appId: process.env.REACT_APP_APPID, 
 };
 
-const firebaseConfigTesting = {
-    apiKey: "AIzaSyA5bBx4AirIyDeXjSvblm9FLJooSzggQUs",
-    authDomain: "react-test-app-curso.firebaseapp.com",
-    projectId: "react-test-app-curso",
-    storageBucket: "react-test-app-curso.appspot.com",
-    messagingSenderId: "805099183686",
-    appId: "1:805099183686:web:6c224813eb7893721db447"
-  };
-
-if(process.env.NODE_ENV === 'test') {
-    // testing
-    firebase.initializeApp(firebaseConfigTesting);
-
-} else{
-    // dev/prod
-    firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
 
 
 /* es la base de datos */
