ed2919e34a77b3d7acfd5dc5a9414135428caea0
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-04-12 17:36:26 -0500

    Acciones startLogout y startLoginEmailPassword

--- journal-app/src/tests/actions/auth.test.js
+++ journal-app/src/tests/actions/auth.test.js
@@ -1,7 +1,22 @@
-import { login, logout } from "../../actions/auth";
+import { createStore } from 'redux';
+import configureStore from 'redux-mock-store'; 
+import thunk from 'redux-thunk';
+
+import { login, logout, startLoginEmailPassword, startLogout } from "../../actions/auth";
 import { types } from "../../types/types";
 
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+const initState = {};
+
+let store = mockStore(initState);
+
 describe('Pruebas con las acciones de auth', () => {
+
+    beforeEach(() => {
+        store = mockStore(initState);
+    })
 
     test('Login y Logout deben de crear la acción respectiva ', () => {
 
@@ -24,5 +39,39 @@ describe('Pruebas con las acciones de auth', () => {
         })
 
     });
+
+    test('Debe realiar el logout ', async () => {
+        
+        await store.dispatch(startLogout());
+
+        const actions = store.getActions();
+    
+        expect(actions[0]).toEqual({
+            type: types.logout
+        });
+
+        expect(actions[1]).toEqual({
+            type: types.notesLogoutCleaning
+        });
+
+    });
+
+    test('Debe iniciar el startLoginEmailPassword ', async () => {
+        await store.dispatch(startLoginEmailPassword('test@testing.com', '123456'));
+
+        const actions = store.getActions();
+
+        expect(actions[1]).toEqual({
+            type: types.login,
+            payload: {
+                uid: 's8P1V1rL8VeVPRuey48sd8MZTn82',
+                displayName: null
+            }
+        })
+
+    });
+    
+
+    
 
 })
