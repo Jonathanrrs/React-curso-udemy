70d61d8112ffc3da1f6eed2e60670dfb16d83979
jonathan <yoni_12rrs@hotmail.com>
2021-07-28 21:04:10 -0500

    Eliminar evento

--- 10-calendar-backend/controllers/events.js
+++ 10-calendar-backend/controllers/events.js
@@ -34,8 +34,6 @@ const crearEvento = async (req, res = response) => {
             msg: 'Hable con el administrador'
         })
     }
-    
-  
 };
 
 const actualizarEvento = async (req, res = response) => {
@@ -47,7 +45,7 @@ const actualizarEvento = async (req, res = response) => {
         
         const evento = await Evento.findById(eventoId);
         if(!evento) {
-            res.json.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Evento no existe por ese id'
             });
@@ -85,10 +83,42 @@ const actualizarEvento = async (req, res = response) => {
 };
 
 const eliminarEvento = async (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'eliminarEvento'
-    });
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const evento = await Evento.findById(eventoId);
+        if(!evento) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Evento no existe por ese id'
+            });
+        }
+
+        /* necesario hacer el toString */
+        if(evento.user.toString() !== uid) {
+            return res.status(401).json({
+              ok: false,
+              msg: 'No tiene privilegio de eliminar este evento'  
+            });
+        }
+
+    
+        /* que retorne los datos actualizados nuevos, por eso el new */
+         await Evento.findByIdAndDelete(eventoId);
+
+        res.json({
+            ok: true,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        });
+    }
 };
 
 module.exports = {
