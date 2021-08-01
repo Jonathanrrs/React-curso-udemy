872c5b6427d61133278ba38e3ce00f9504d5a99e
jonathan <yoni_12rrs@hotmail.com>
2021-08-01 15:39:07 -0500

    Mostrar ruta pública o privada si se está autenticado o no

--- 09-calendar-app/src/tests/router/AppRouter.test.js
+++ 09-calendar-app/src/tests/router/AppRouter.test.js
@@ -7,31 +7,77 @@ import '@testing-library/jest-dom';
 import { AppRouter } from '../../router/AppRouter';
 
 
-
-
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const initState = {
-    auth: {
-        checking: true
-    }
-};
-const store = mockStore(initState);
 // store.dispatch = jest.fn();
 
-
-
 describe('Pruebas en AppRouter', () => {
-   test('Debe mostrar el espere... ', () => {
+    test('Debe mostrar el espere... ', () => {
 
-    const wrapper = mount(
-        <Provider store={store}>
-            <AppRouter />
-        </Provider>
-    );
-    expect(wrapper).toMatchSnapshot();
-});
+        const initState = {
+            auth: {
+                checking: true
+            }
+        };
 
+        const store = mockStore(initState);
+
+        const wrapper = mount(
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('Debe mostrar la ruta pública ', () => {
+
+        const initState = {
+            auth: {
+                checking: false,
+                uid: null
+            }
+        };
     
+        const store = mockStore(initState);
+    
+        const wrapper = mount(
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.login-container').exists()).toBe(true);
+    });
+
+    test('Debe mostrar la ruta privada ', () => {
+
+        const initState = {
+            ui: {
+                modalOpen: false
+            },
+            calendar: {
+                events:  []
+            },
+            auth: {
+                checking: false,
+                uid: '123',
+                name: 'test'
+            }
+        };
+    
+        const store = mockStore(initState);
+    
+        const wrapper = mount(
+            <Provider store={store}>
+                <AppRouter />
+            </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.calendar-screen').exists()).toBe(true);
+    });
+
+
+
 });
